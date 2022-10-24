69d06d9d1fab104509811dde7602b017db2e6c0e
Ben Noble <benjamin4504@gmail.com>
2022-10-23 21:07:51 -0400

    adds shape 3 to background

--- main.js
+++ main.js
@@ -15,6 +15,7 @@ const shadeOne = document.querySelector('#shade1');
 const shadeTwo = document.querySelector('#shade2');
 const shapeOne = document.querySelector('#shape1');
 const shapeTwo = document.querySelector('#shape2');
+const shapeThree = document.querySelector('#shape3');
 
 window.addEventListener('scroll', function () {
     // let scrollPositionY = window.pageYOffset;
@@ -24,5 +25,6 @@ window.addEventListener('scroll', function () {
     shadeTwo.style.transform = 'translateX(' + scrollPositionX * .4 + 'px)';
     shapeOne.style.transform = 'translateX(' + scrollPositionX * -.5 + 'px)' + 'translateY(' + scrollPositionX * -.5 + 'px)';
     shapeTwo.style.transform = 'translateX(' + scrollPositionX * .4 + 'px)' + 'translateY(' + scrollPositionX * .7 + 'px)';
+    // shapeThree.style.transform = 'translateX(' + scrollPositionX * .4 + 'px)' + 'translateY(' + scrollPositionX * .7 + 'px)';
 
 });
